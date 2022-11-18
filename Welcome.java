ff3d8a90135912d5e1f2a4b0ab01300334b29066
neelwrites <sunilwrites@gmail.com>
2022-11-18 09:41:23 +0530

    java update

--- Welcome.java
+++ Welcome.java
@@ -6,6 +6,6 @@ public class Welcome
 {
     public static void main( String[] args )
     {
-        System.out.println( "Welcome World from Jenkins New Commit!" );
+        System.out.println( "Welcome World from Jenkins New1 Commit!" );
     }
 }
